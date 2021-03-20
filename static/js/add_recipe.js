cfb69e38dad900f037b681ef26692fc708293d85
Kdoggg666 <kenanwright@gmail.com>
2021-03-20 15:37:18 +0000

    Add sidenav styles

--- static/js/add_recipe.js
+++ static/js/add_recipe.js
@@ -4,13 +4,13 @@ let ing = 1;
 
 $(".add_field_button").click(function () {
     ing++;
-    $(".ingredients_list").append('<div class="input-field col s12 "><input id="ing_name' + ing + '" type="text" name="ing_name"/><label class="black-text" for="ing_name' + ing + '">Ingredient ' + ing + '</label><i id="remove_field" class="btn fas fa-minus red right"></i></div></div>');
+    $(".ingredients_list").append('<div class="input-field col s12 "><input id="ing_name' + ing + '" type="text" name="ing_name"/><label class="white-text" for="ing_name' + ing + '">Ingredient ' + ing + '</label><i id="remove_field" class="btn fas fa-minus red right"></i></div></div>');
 
 });
 
 $(".add_step_button").click(function () {
     step++;
-    $(".step_list").append('<div class="input-field col s12 "><input type="text" name="steps"/><label class="black-text" for="steps">Step ' + step + '</label><i id="remove_step" class="btn fas fa-minus red right"></i></div></div>');
+    $(".step_list").append('<div class="input-field col s12 "><input type="text" name="steps"/><label class="white-text" for="steps">Step ' + step + '</label><i id="remove_step" class="btn fas fa-minus red right"></i></div></div>');
     
 });
 
