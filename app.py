c4a689b0b8ff295e1edb754d2ec5be11a4bd5f56
Kdoggg666 <kenanwright@gmail.com>
2021-04-04 19:02:59 +0000

    Clean up app.py code indentation for pep8 compliance

--- app.py
+++ app.py
@@ -199,11 +199,13 @@ def login():
         if existing_user:
             # ensure hashed password matches user input
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    session["name"] = existing_user["name"]
-                    flash("Welcome, {}!".format(request.form.get("username")))
-                    return redirect(url_for("user_profile", username=session["user"]))
+                                    existing_user["password"],
+                                    request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                session["name"] = existing_user["name"]
+                flash("Welcome, {}!".format(request.form.get("username")))
+                return redirect(url_for("user_profile",
+                                        username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
